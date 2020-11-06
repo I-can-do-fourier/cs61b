c46c79a38afa9c7f9774e8828761ff3df6f1668e
han xinhang <noname98@126.com>
2020-11-06 23:01:47 +0800

    setup the background

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -2,6 +2,36 @@
 
 public class NBody{
 
+
+	public static void main(String[] args) {
+		
+      //read the information of the universe
+		double T=Double.parseDouble(args[0]);
+
+        double dt=Double.parseDouble(args[1]);
+
+        String filename=args[2];
+
+        In in=new In(filename);
+
+        int n=in.readInt();
+
+        Double r=in.readDouble();
+
+
+      //draw the background of the universe
+
+
+
+        StdDraw.setCanvasSize(1000,1000);
+        StdDraw.picture(0.5,0.5,"./images/starfield.jpg");
+
+
+        
+
+         
+	}
+
 	
 
 
