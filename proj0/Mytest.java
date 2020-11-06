e22782adfd5f928e964a945a3720a3d41868c30c
han xinhang <noname98@126.com>
2020-11-06 21:01:17 +0800

    finish the readradius for nbody

---
+++ proj0/Mytest.java
@@ -0,0 +1,33 @@
+
+
+//this is the test of myself
+
+public class Mytest{
+
+
+   public static void main(String[] arg){
+
+
+   			testforpairwiseforce();
+
+
+
+   }
+
+
+
+
+
+   public static void testforpairwiseforce(){
+
+
+           
+
+
+   }
+
+
+
+}
+
+
---
+++ proj0/NBody.java
@@ -0,0 +1,25 @@
+
+
+public class NBody{
+
+	
+
+
+  public static double readRadius(String s){
+
+        In in=new In(s);
+
+
+        int number=in.readInt();
+
+        Double r_universe=in.readDouble();
+       
+        return r_universe;
+
+  }
+
+
+
+
+
+}
