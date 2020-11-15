ffc61dacb4a9ab49067d6013f6362429ec243f52
han xinhang <noname98@126.com>
2020-11-15 22:38:57 +0800

    test for integer and int

--- proj1gold/int_text.java
+++ proj1gold/int_text.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class int_text {
+
+    public static void main(String[] args) {
+
+
+        int a=2;
+
+        Integer b=a;
+
+        sb(2);
+
+        int c=sb(2);
+
+        Integer d=sb(3);
+
+        int e=sb1(2);
+
+    }
+
+
+
+
+    static int sb(Integer x){
+
+
+        return x;
+
+
+    }
+
+    static Integer sb1(Integer x){
+
+        return x;
+
+
+
+    }
 }
