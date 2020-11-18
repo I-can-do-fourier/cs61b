9b001abc41476047aff6cc23ebd21084766f541c
han xinhang <noname98@126.com>
2020-11-18 10:47:35 +0800

    finish the lab5

--- proj2/byog/lab5/HexWorld_tessellated.java
+++ proj2/byog/lab5/HexWorld_tessellated.java
@@ -1,18 +1,163 @@
 package byog.lab5;
 import org.junit.Test;
+
+//import static byog.lab5.Hex_test.*;
 import static org.junit.Assert.*;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
-public class HexWorld_tessellated {
+public class HexWorld_tessellated extends Hex_test{
 
+
+    private static final int WIDTH = 150;
+    private static final int HEIGHT = 150;
+
+    private static final long SEED = 12432;
+    private static final Random RANDOM = new Random(SEED);
+
+    public static void main(String[] args) {
+
+
+
+        TERenderer ter = new TERenderer();
+
+        TETile[][] world=initialization_world(WIDTH,HEIGHT,ter);
+
+
+        tessellated_launch(3,2,world);
+
+        ter.renderFrame(world);
+
+
+    }
+
+
+
+
+  public static void tessellated_launch(int size_singlehex,int  size_bighex, TETile[][] world){
+
+
+                List<int[]> positions=Hex_settings(size_singlehex,size_bighex);
+
+                for(int[] position:positions){
+
+                    addHexagon(position[0],position[1],size_singlehex,world);
+
+                }
+
+  }
+
+  //calculate the position of each hex;
+
+  public static List<int[]> Hex_settings(int size_singlehex, int size_bighex){
+
+             List<int[]> positions=new ArrayList<>();
+
+
+             int x_init=WIDTH/2-size_singlehex;
+             int y_init=HEIGHT/2+1;
+
+             boolean iterate_pattern=true;
+
+            //create the head part of the bighex
+             for(int i=1;i<size_bighex;i++){
+
+                for(int j=1;j<=i;j++) {
+                    positions.add(new int[]{x_init - (i - 1) * (2 * size_singlehex - 1) + (j - 1) * (4 * size_singlehex - 2), y_init + (2 * size_bighex - 2 - i + 1) * (size_singlehex)});
+                }
+
+             }
+
+
+             //create the body of the bighex
+
+              for(int i=1;i<=size_bighex;i++){
+
+
+                     for(int j=1;j<=size_bighex;j++) {
+
+                         positions.add(new int[]{x_init-(size_bighex-1)*(2*size_singlehex-1)+(j-1)*(4*size_singlehex-2), y_init + (size_bighex - 1 - 2*i + 2) * (size_singlehex)});
+
+                     }
+
+
+
+                  }
+
+
+
+                for(int i=1;i<=size_bighex-1;i++) {
+                    for (int j = 1; j <= size_bighex - 1; j++) {
+
+                        positions.add(new int[]{x_init - (size_bighex - 2) * (2 * size_singlehex - 1) + (j - 1) * (4 * size_singlehex - 2), y_init + (size_bighex - 2 - 2*i + 2) * (size_singlehex)});
+
+                    }
+
+
+                }
+
+
+                for(int i=1;i<size_bighex;i++){
+
+                    for(int j=1;j<=i;j++) {
+                        positions.add(new int[]{x_init - (i - 1) * (2 * size_singlehex - 1) + (j - 1) * (4 * size_singlehex - 2), y_init-(2 * size_bighex - 2 - i + 1) * (size_singlehex)});
+                    }
+
+                }
+
+
+
+              return positions;
+
+  }
+
+
+    public static void addHexagon(int x_position,int y_position, int size,TETile[][] world){
+
+
+
+        TETile t=randomTile();
+
+        for(int[] position:Hex_creation(size)){
+
+            try {
+
+                world[position[0] + x_position][position[1] + y_position] =t;
+
+            }catch(ArrayIndexOutOfBoundsException e ){
+
+
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+  private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(5);
+        switch (tileNum) {
+            case 0: return Tileset.WALL;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.TREE;
+            case 3: return Tileset.SAND;
+            case 4: return Tileset.UNLOCKED_DOOR;
+            default: return Tileset.NOTHING;
+        }
+    }
 
 
 }
--- proj2/byog/lab5/Hex_test.java
+++ proj2/byog/lab5/Hex_test.java
@@ -1,4 +1,131 @@
 package byog.lab5;
 
+
+//create single hexagon
+
+import byog.TileEngine.TERenderer;
+import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hex_test {
+
+
+      private static final int WIDTH = 50;
+      private static final int HEIGHT = 50;
+
+
+
+    public static void main(String[] args) {
+
+        TERenderer ter = new TERenderer();
+
+        TETile[][] world=initialization_world(WIDTH,HEIGHT,ter);
+
+        addHexagon(34,3,3,world);
+
+        ter.renderFrame(world);
+
+    }
+
+
+   public static TETile[][] initialization_world(int WIDTH,int HEIGHT,TERenderer ter){
+
+                //TERenderer ter = new TERenderer();
+                ter.initialize(WIDTH, HEIGHT);
+
+
+                TETile[][] world = new TETile[WIDTH][HEIGHT];
+
+
+                blank_world(world,WIDTH,HEIGHT);
+
+                return world;
+
+    }
+
+    public static void blank_world(TETile[][] world,int WIDTH,int HEIGHT){
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+
+    }
+
+
+    //add a Hexagon to the digital world
+
+    public static void addHexagon(int x_position,int y_position, int size,TETile[][] world){
+
+
+
+
+        for(int[] position:Hex_creation(size)){
+
+
+
+
+          try {
+              world[position[0] + x_position][position[1] + y_position] = Tileset.a;
+
+          }catch(ArrayIndexOutOfBoundsException e ){
+
+
+
+
+          }
+
+
+        }
+
+
+
+    }
+
+
+    //create the hexagon
+
+    public static List<int[]> Hex_creation(int size){
+
+           List<int[]> positions =new ArrayList<>();
+
+           for(int i=0;i<size;i++) {
+
+               for (int j = 0; j < size + i * 2; j++) {
+
+                   positions.add(new int[]{(size - 1 - i + j), (size - 1 - i)});
+
+
+               }
+
+           }
+
+           for(int i=0;i<size;i++){
+
+                for(int j=0;j<size+i*2;j++){
+
+                   positions.add(new int[]{(size-1-i+j),-(size-1-i)-1});
+
+
+                }
+
+
+           }
+
+           return positions;
+
+
+    }
+
+
+
+
+
+
+
 }
